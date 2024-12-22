1261292975c04794d217551c775441c59bbedc23
SUJAIT <sujait2000@.com>
2024-12-22 19:22:45 +0600

    Added-work-Sujait

---
+++ src/Components/Authentication/Firebase.config.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmxouwKs_46-pdzhvpsj1ryuaPey5yOJQ",
+  authDomain: "busadmin-46db1.firebaseapp.com",
+  projectId: "busadmin-46db1",
+  storageBucket: "busadmin-46db1.appspot.com",
+  messagingSenderId: "1083814862680",
+  appId: "1:1083814862680:web:8baac4d2c5d6f41a41cf2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
